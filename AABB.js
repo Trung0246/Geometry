852495527256cb80b3e75a4d1673701221996b42
tsh96 <tansiahow1996@gmail.com>
2016-08-07 15:30:04 +0800

    Change the structure of AABB

--- AABB.js
+++ AABB.js
@@ -13,17 +13,17 @@
  *  - Quadratic bezier curve
  *  - Cubic bezier curve
  *
- * @param x {number} x value of top left corner
- * @param y {number} y value of top left corner
- * @param width {number} width of the bound
- * @param height {number} height of the bound
+ * @param top {number} top boundary
+ * @param right {number} right boundary
+ * @param bottom {number} bottom boundary
+ * @param left {number} left boundary
  * @constructor
  */
-function AABB(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+function AABB(top, right, bottom, left) {
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+    this.left = left;
 }
 
 /**
@@ -35,46 +35,98 @@ function AABB(x, y, width, height) {
  * @returns {AABB}
  */
 AABB.fromSegment = function (x1, y1, x2, y2) {
-    return new AABB(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
+    return new AABB(Math.min(y1, y2), Math.max(x1, x2), Math.max(y1, y2), Math.min(x1, x2));
 };
 
 /**
- * create the AABB from circle (a circle with two end point)
+ * create the AABB from circle
  * @param cx {number}
  * @param cy {number}
  * @param r {number}
  * @return {AABB}
  */
 AABB.fromCircle = function (cx, cy, r) {
-    return new AABB(cx - r, cy - r, r * 2, r * 2);
+    return new AABB(cy - r, cx + r, cy + r, cx - r);
 };
 
-AABB.fromEllipse = function (cx, cy, a1, b1, c1) {
+/**
+ * create the AABB from ellipse
+ * x = cx + a cos(c) cos(t) - b sin(c) sin(t) --- (1)
+ * y = cy + a sin(c) cos(t) + b cos(c) sin(t) --- (2)
+ * @param cx {number}
+ * @param cy {number}
+ * @param a {number}
+ * @param b {number}
+ * @param c {number}
+ * @return {AABB}
+ */
+AABB.fromEllipse = function (cx, cy, a, b, c) {
     /**
-     * x = x1 + a1 cos(c1) cos(t) - b1 sin(c1) sin(t) --- (1)
-     * y = y1 + a1 sin(c1) cos(t) + b1 cos(c1) sin(t) --- (2)
-     *
-     * Differentiate x and y in term of t
-     * dx/dt = -a1 cos(c1) sin(t) - b1 sin(c1) cos(t) --- (3)
-     * dy/dt = -a1 sin(c1) sin(t) + b1 cos(c1) cos(t) --- (4)
-     *
+     * Step 1: Simplify the equation
      * let:
-     * a = a1 cos(c1)
-     * b = -b1 sin(c1)
-     * c = a1 sin(c1)
-     * d = b1 cos(c1)
+     * d = a cos(c)
+     * e = -b sin(c)
+     * f = a sin(c)
+     * g = b cos(c)
+     * Then:
+     * x = cx + d cos(t) + e sin(t) --- (3)
+     * y = cy + f cos(t) + g sin(t) --- (4)
+     */
+    var d = a * Math.cos(c);
+    var e = -b * Math.sin(c);
+    var f = a * Math.sin(c);
+    var g = b * Math.cos(c);
+
+    /**
+     * Step 2: Differentiate x and y in term of t
+     * dx/dt = -d sin(t) + e cos(t) --- (5)
+     * dy/dt = -f sin(t) + g cos(t) --- (6)
+     *
      *
      * Apply chain rule to find the dy/dx
      * dy/dx = dy/dt * dt/dx
      * dy/dx = dy/dt * 1/(dx/dt)
+     */
+
+    /**
+     * Step 3: Find the top and bottom boundary
+     * The top and bottom boundary of the ellipse are located at dy/dx = 0, So,
+     * 0 = dy/dt * 1/(dx/dt) --- Then
+     * dy/dt = 0 --- Substitute (6)
+     * -f sin(t) + g cos(t) = 0 --- Apply substitution of tangent half angle
+     * -f ((2 x) / (x^2 + 1)) + g ((1 - x^2) / (x^2 + 1)) = 0 --- Simplify
+     * g - 2 f x - g x^2 = 0 --- Solve the x
      *
-     * The highest point and the lowest point of the ellipse are located at dy/dx = 0, So,
-     * 0 = dy/dx * 1/(dx/dt) --- Then
-     * dy/dt = 0 --- Substitute (4)
-     * -a1 sin(c1) sin(t) + b1 cos(c1) cos(t) = 0 --- Apply substitution of tangent half angle
-     * -a1 sin(c1) ((2 x) / (x^2 + 1)) + b1 cos(c1) ((1 - x^2) / (x^2 + 1)) = 0
-     *
-     *
+     * x = (-f - Sqrt[f^2 + g^2])/g && (-f + Sqrt[f^2 + g^2])/g
+     * Then:
+     * t = 2 atan(x)
+     * top and bottom boundary = cy + f cos(t) + g sin(t)
      *
      */
+    var sqrtD = Math.sqrt(f * f + g * g);
+    var t1 = 2 * Math.atan((-f - sqrtD) / g);
+    var t2 = 2 * Math.atan((-f + sqrtD) / g);
+    var y1 = cy + f * Math.cos(t1) + g * Math.sin(t1);
+    var y2 = cy + f * Math.cos(t2) + g * Math.sin(t2);
+
+    /**
+     * Step 4: Find the left and right boundary
+     * The left and right boundary of the ellipse are located at dy/dx = infinity, So,
+     * infinity = dy/dt * 1/(dx/dt) --- Then
+     * dx/dt = 0 --- Substitute (5)
+     * -d sin(t) + e cos(t) = 0 --- Apply substitution of tangent half angle
+     * -d ((2 x) / (x^2 + 1)) + e ((1 - x^2) / (x^2 + 1)) = 0 --- Simplify
+     * e - 2 d x - e x^2 = 0 --- Solve the x
+     * x = (-d - Sqrt[d^2 + e^2])/e && (-d + Sqrt[d^2 + e^2])/e
+     * Then:
+     * t = 2 atan(x)
+     * left and right boundary = cx + d cos(t) + e sin(t)
+     */
+    sqrtD = Math.sqrt(d * d + e * e);
+    t1 = 2 * Math.atan((-d - sqrtD) / e);
+    t2 = 2 * Math.atan((-d + sqrtD) / e);
+    var x1 = cx + d * Math.cos(t1) + e * Math.sin(t1);
+    var x2 = cx + d * Math.cos(t2) + e * Math.sin(t2);
+
+    return new AABB(Math.min(y1, y2), Math.max(x1, x2), Math.max(y1, y2), Math.min(x1, x2));
 };
